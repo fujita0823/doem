36a21125cd73d589a60b11d34aa27ef641c72712
fujita0823 <hkrfjt42144@gmail.com>
2023-12-02 02:20:54 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DATSET="capella"
+OUTDIR="results"
+SEED=0
+EPOCHS=100
+
+# depend on the experiments
+train_options="--use_pe --rotate 1"
+log_options="--wandb --log_plt --log_fig"
+
+# train
+python3 train_unet_capella.py --dataset $DATSET --outdir $OUTDIR --seed $SEED --n_epochs $EPOCHS $train_options $log_options
+
+# test
+python3 train_unet_capella.py --dataset $DATSET --outdir $OUTDIR --seed $SEED --n_epochs $EPOCHS --testmode $train_options $log_options

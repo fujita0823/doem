c87a315e719cbf8cb3019175b55e6bb9db2ffea5
fujita0823 <hkrfjt42144@gmail.com>
2023-12-08 15:42:40 +0900

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-DATSET="capella"
-OUTDIR="results"
+DATSET="../oem/data/capella-oem"
+OUTDIR="../oem/data/results/"
 SEED=0
 EPOCHS=100
 
 # depend on the experiments
-train_options="--use_pe --rotate 1"
-log_options="--wandb --log_plt --log_fig"
+train_options="--rotate 1"
+log_options="--log_plt --log_fig"
+wandb="--wandb"
+network="--network unetformer"
 
 # train
-python3 train_unet_capella.py --dataset $DATSET --outdir $OUTDIR --seed $SEED --n_epochs $EPOCHS $train_options $log_options
+python3 train_unet_capella.py --dataset $DATSET --outdir $OUTDIR --seed $SEED --n_epochs $EPOCHS $train_options $log_options $wandb $network
 
 # test
-python3 train_unet_capella.py --dataset $DATSET --outdir $OUTDIR --seed $SEED --n_epochs $EPOCHS --test_mode $train_options $log_options
+python3 train_unet_capella.py --dataset $DATSET --outdir $OUTDIR --seed $SEED --n_epochs $EPOCHS --test_mode $train_options $log_options $network
